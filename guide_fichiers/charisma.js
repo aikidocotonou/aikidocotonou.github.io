8bd9f75b133f5f91cba0a9084f739d4a8ea57a8e
aikidocotonou <issegnondjivoh@gmail.com>
2024-06-06 13:46:09 +0200

    Add files via upload

---
+++ guide_fichiers/charisma.js
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html>
+
+    <head>
+        <title>404 Error - Page Not Found</title>
+        <style>
+            #ad_frame{ height:800px; width:100%; }
+            body{ margin:0; border: 0; padding: 0; }
+        </style>
+        <script src="//web.archive.org/web/20181126215711/http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script type="text/javascript" language="JavaScript">
+            var url = 'https://web.archive.org/web/20181126215711/http://www.iyfipgun.com/?dn='
+                + document.domain + '&pid=9POL6F2H4';
+
+            $(document).ready(function() {
+                $('#ad_frame').attr('src', url);
+            });
+        </script>
+    </head>
+    <body>
+        <iframe id="ad_frame" src="https://web.archive.org/web/20181126215711/http://www.iyfipgun.com/"
+            frameborder="0" scrolling="no">
+
+            <!-- browser does not support iframe's -->
+
+        </iframe>
+    </body>
+
+</html>
+<!--
+     FILE ARCHIVED ON 21:57:11 Nov 26, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:45:19 Jun 05, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
+<!--
+playback timings (ms):
+  captures_list: 0.601
+  exclusion.robots: 0.071
+  exclusion.robots.policy: 0.06
+  esindex: 0.009
+  cdx.remote: 5.657
+  LoadShardBlock: 260.337 (3)
+  PetaboxLoader3.datanode: 263.139 (5)
+  PetaboxLoader3.resolve: 154.548 (3)
+  load_resource: 167.62 (2)
+-->
